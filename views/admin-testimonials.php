c1114759470e60c1ff3f744e67558d80f03656b4
adit <bangke0016@example.com>
2026-09-10 17:10:52 +0800

    tes auto deploy

--- views/admin-testimonials.php
+++ views/admin-testimonials.php
@@ -4,7 +4,7 @@
 <head>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width,initial-scale=1">
-<title>Testimoni | Velocity Admin</title>
+<title>TestimoniI | Velocity Admin</title>
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500;600;700&family=Space+Grotesk:wght@500;600;700&display=swap" rel="stylesheet">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
